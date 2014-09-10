13dbbf98d1c1f425fe06f7341e751cf91ca8764f
jVillaveces <secevalliv@gmail.com>
2014-09-10 13:05:13 +0200

    parser and tests

---
+++ index.js
@@ -0,0 +1,118 @@
+var _ = require('underscore');
+
+module.exports = MITab = (function() {
+     
+    var nodes = {}; 
+    var links = [];
+    var scores = {};
+    var textInParenthesis = /\((.*?)\)/;
+    var textInQuotes = /\"(.*?)\"/;
+    var textInTax = /\:(.*?)\(/;
+    
+    // Creates a node from identifiers, alternative idenifiers,
+    // and taxonomy values
+    var _getNode = function(idStr, altIdsStr, taxStr){
+        var ids = _.map(idStr.split('|'), _mapPub);
+        var node = {
+            id: ids[0].value,
+            ids: ids,
+            altIds: _.map(altIdsStr.split('|'), _mapPub),
+            taxonomy: _.uniq(_.map(taxStr.split('|'), _mapTaxonomy))
+        }
+        return node;
+    }
+    
+    // Parses a string and returns an interaction
+    var _parse = function(line, i){
+        
+        var fields = line.split('\t');
+        
+        var nodeA = _getNode(fields[0], fields[2], fields[9]);
+        var nodeB = _getNode(fields[1], fields[3], fields[10]);
+        
+        
+        var interaction = {
+            source: nodeA.id,
+            target: nodeB.id,
+            detMethods: _.map(fields[6].split('|'), _mapField),
+            firstAuthor: fields[7].split('|'),
+            publications: _.map(fields[8].split('|'), _mapPub),
+            intTypes: _.map(fields[11].split('|'), _mapField),
+            sourceDbs: _.map(fields[12].split('|'), _mapField),
+            intIds : _.map(fields[13].split('|'), _mapPub),
+            scores: _.map(fields[14].split('|'), _mapScore)
+        };
+        
+        nodes[nodeA.id] = nodeA;
+        nodes[nodeB.id] = nodeB;
+        
+        return interaction;
+    }
+    
+    var _mapScore = function(scoreStr){
+        var arr = scoreStr.split(':'), score = {name:arr[0], score:+arr[1]};
+        
+        _addScore(score);
+        return score;
+    };
+    
+    var _mapField = function(fieldStr){
+        if(fieldStr.match(textInQuotes) == null || fieldStr.match(textInParenthesis) == null){
+            var arr = fieldStr.split(':');
+            return {name:arr[0], score:arr[1]};
+        }
+        return {name:fieldStr.match(textInQuotes)[1], value:fieldStr.match(textInParenthesis)[1]};
+    };
+    
+    var _mapPub = function(pubStr){
+        var arr = pubStr.split(':');
+        return {name:arr[0], value:arr[1]};
+    };
+    
+    var _mapTaxonomy = function(taxStr){
+        if(taxStr != '-'){
+            return (taxStr.match(textInTax) == null) ? taxStr.split(':')[1] : taxStr.match(textInTax)[1];
+        }
+    };
+    
+    var _addScore = function(score){
+        if( !_.isNaN(score.score) && _.isNumber(score.score)){
+            if(_.has(scores, score.name)){
+                if(scores[score.name].min > score.score) scores[score.name].min = score.score;
+                if(scores[score.name].max < score.score) scores[score.name].max = score.score;
+            }else{
+                scores[score.name] = {name:score.name, min:score.score, max:score.score};
+            }
+        }
+    };
+    
+    function MITab() {}
+    
+    MITab.parse = function(text) {
+        
+        if (! _.isString(text)) {
+            throw 'MITab cannot parse ' + typeof text;
+            return;
+        }
+        
+        // Initialize variables
+        nodes = {}; 
+        links = [];
+        scores = {};
+        
+        var lines = text.split('\n');
+        
+        var interactions = _.map(lines, _parse)
+        var nodeval = _.values(nodes);
+        
+        return {
+            links: interactions,
+            nodes: nodeval,
+            ids: _.pluck(nodeval, 'id'),
+            taxa: _.compact(_.reduce(nodeval, function(memo,n){ return _.union(memo,n.taxonomy)},[])),
+            scores: _.values(scores)
+        };
+    };
+    
+    return MITab;
+})();
---
+++ test/test.js
@@ -0,0 +1,178 @@
+//Require chai assert for test
+var assert = require('chai').assert;
+
+// Require fs to get interactions from file
+var fs = require('fs');
+
+//Require MITab parser client 
+var mitab = require('../');
+
+// {ath to the interactions file
+var path = 'test/interactions.txt';
+
+// Variable holding the interactions in the file
+var intStr;
+
+// Reading the interactions file
+describe('Read file', function(){
+    it('should read without error', function(done){
+        fs.readFile(path, 'utf8', function (err, data) {
+            if (err) throw err;
+            intStr = data;
+            done();
+        });
+    });
+});
+
+// Test data
+describe('Check file contents', function(){
+
+    it('should be a string', function(){
+        assert.isString(intStr, 'data is a string');
+    });
+    
+});
+
+var intObj;
+
+// Test the parser
+describe('Parse Interactions', function(){
+
+    it('should throw error', function(){
+        assert.throws(mitab.parse, {});
+    });
+    
+    it('should parse the interactions', function(){
+        intObj = mitab.parse(intStr);
+    });
+    
+    it('should be an object', function(){
+        assert.isObject(intObj);
+    });
+});
+
+describe('Check nodes', function(){
+
+    it('should be an array', function(){
+        assert.isArray(intObj.nodes);
+    });
+    
+    it('should contain four nodes', function(){
+        assert.equal(intObj.nodes.length, 4);
+    });
+});
+
+describe('Check first node', function(){
+    
+    it('should be an Object', function(){
+        assert.isObject(intObj.nodes[0]);
+    });
+    
+    it('should have id Q82235', function(){
+        assert.equal(intObj.nodes[0].id, 'Q82235');
+    });
+    
+    it('should have ids array', function(){
+        assert.isArray(intObj.nodes[0].ids);
+    });
+    
+    it('should have altIds array', function(){
+        assert.isArray(intObj.nodes[0].altIds);
+    });
+    
+    it('should have taxonomy array', function(){
+        assert.isArray(intObj.nodes[0].taxonomy);
+    });
+});
+
+describe('Check interactions', function(){
+    
+    it('should be an array', function(){
+        assert.isArray(intObj.links);
+    });
+    
+    it('should contain two interactions', function(){
+        assert.equal(intObj.links.length, 2);
+    });
+});
+
+describe('Check first interaction', function(){
+    
+    it('should be an Object', function(){
+        assert.isObject(intObj.links[0]);
+    });
+    
+    it('should have source Q82235', function(){
+        assert.equal(intObj.links[0].source, 'Q82235');
+    });
+    
+    it('should have target Q9DBG9', function(){
+        assert.equal(intObj.links[0].target, 'Q9DBG9');
+    });
+    
+    it('should have firstAuthor Kanamori et al. (2003)', function(){
+        assert.equal(intObj.links[0].firstAuthor, 'Kanamori et al. (2003)');
+    });
+    
+    it('should have publications array', function(){
+        assert.isArray(intObj.links[0].publications);
+    });
+    
+    it('should have intTypes array', function(){
+        assert.isArray(intObj.links[0].publications);
+    });
+    
+    it('should have sourceDbs array', function(){
+        assert.isArray(intObj.links[0].publications);
+    });
+    
+    it('should have intIds array', function(){
+        assert.isArray(intObj.links[0].publications);
+    });
+    
+    it('should have scores array', function(){
+        assert.isArray(intObj.links[0].publications);
+    });
+});
+
+describe('Check ids', function(){
+
+    it('should be an array', function(){
+        assert.isArray(intObj.ids);
+    });
+    
+    it('should contain four ids', function(){
+        assert.equal(intObj.ids.length, 4);
+    });
+    
+    it('should be [Q82235, Q9DBG9, P04578, B0FAM1]', function(){
+        assert.deepEqual(intObj.ids, ['Q82235', 'Q9DBG9', 'P04578', 'B0FAM1']);
+    });
+});
+
+describe('Check taxa', function(){
+
+    it('should be an array', function(){
+        assert.isArray(intObj.taxa);
+    });
+    
+    it('should contain four taxa', function(){
+        assert.equal(intObj.taxa.length, 4);
+    });
+});
+
+describe('Check scores', function(){
+
+    it('should be an array', function(){
+        assert.isArray(intObj.scores);
+    });
+    
+    it('should contain one score', function(){
+        assert.equal(intObj.scores.length, 1);
+    });
+    
+    it('should have min one score', function(){
+        assert.equal(intObj.scores.length, 1);
+    });
+});
+
